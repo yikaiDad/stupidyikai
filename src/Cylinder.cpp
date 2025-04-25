09ba4124cf10b5d7dec99d8db9e3e1197325106b
yikaiDad <jerrywei.ee13@nycu.edu.tw>
2025-04-25 16:28:58 +0800

    complete part1 of Cylinder.cpp

--- src/Cylinder.cpp
+++ src/Cylinder.cpp
@@ -3,32 +3,33 @@
 
 # include "Cylinder.h"
 #include <cmath> 
+#include <iomanip>
 
 double Cylinder :: SurfaceArea(){
-    return;
+    return 2 * M_PI * radius * radius + 2 * M_PI * radius * height;
 }
 
 double Cylinder :: Volume(){
-    return;
+    return M_PI * height * radius * radius;
 }
 
 double Cylinder :: Circumference(){
-    return;
+    return 2 * M_PI * radius;
 }
 
 istream & operator>>(istream & in, Cylinder & cldr)
 {
-    in >> cldr.radius >> cldr.height;
-    return in;
+    /*in >> cldr.radius >> cldr.height;
+    return in;*/
 }
 
 ostream & operator<<(ostream & out, Cylinder & cldr)
 { 
-    out << fixed << setprecision(3);
+    /*out << fixed << setprecision(3);
     out << "Circumference: " << cldr.Circumference() << endl;
     out << "SurfaceArea: " << cldr.SurfaceArea() << endl;
     out << "Volume: " << cldr.Volume() << endl;
-    return out;
+    return out;*/
 }
 
 # endif
